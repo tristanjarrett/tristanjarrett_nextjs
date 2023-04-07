32ccaddc41939c7b8d0cfc073a5911f29ed11d03
Tristan Jarrett <tristan.jarrett@outlook.com>
2023-04-08 00:11:27 +0100

    Personal template

--- pages/index.js
+++ pages/index.js
@@ -1,124 +1,195 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+import Image from 'next/image';
+import emailjs from 'emailjs-com';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 
-const inter = Inter({ subsets: ['latin'] })
+const Home = () => {
+  const [isSuccess, setIsSuccess] = useState(false); 
 
-export default function Home() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    emailjs
+      .sendForm(
+        'service_ucwym58',
+        'template_mjft6ee',
+        '#contact-form',
+        'tMFc16INtfK2kwXXh'
+      )
+      .then(
+        () => {
+          setIsSuccess(true); // set the state variable to true
+          reset();
+        },
+        (error) => {
+          alert(`An error occurred while sending your message: ${error.text}`);
+        }
+      );
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="bg-gray-100 min-h-screen flex flex-col font-sans">
+      <nav className="bg-gradient-to-r from-blue-500 via-purple-500 to-indigo-500 py-4 px-6 w-full flex justify-between items-center shadow-lg">
+        <div className="flex items-center">
+          <span className="text-xl font-semibold text-white">Tristan Jarrett</span>
         </div>
+        <a href="#contact" className="text-xl font-medium text-white hover:text-blue-300">
+          Get in touch!
+        </a>
+      </nav>
+
+      <div className="container px-4 mx-auto flex-grow">
+        <div className="py-16 lg:py-24 flex flex-col lg:flex-row justify-center items-start lg:space-x-20">
+          <div className="w-full lg:w-2/3">
+            <h1 className="text-5xl font-bold mb-10 lg:pr-20">
+              Hey! I'm <span className="bg-gradient-to-r from-blue-500 via-purple-500 to-indigo-500 bg-clip-text text-transparent">Tristan</span>, a Software Engineer with over
+              <span className="bg-gradient-to-br from-yellow-500 via-orange-500 to-pink-500 bg-clip-text text-transparent"> Eight</span> years of experience.
+            </h1>
+            <p className="text-xl mb-4">
+              I have worked on a wide variety of
+              projects, including website development, mobile app development, SaaS
+              solutions, and Web3 applications that leverage smart contracts and
+              cryptocurrencies. As a seasoned professional, I am dedicated to
+              delivering high-quality work and helping my clients achieve their
+              objectives.
+            </p>
+            <ul className="flex space-x-4 mt-8">
+              <li>
+                <a
+                  href="https://www.linkedin.com/in/tristanjarrett/" className="hover:text-blue-500" target="_blank" rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/tristanjarrett" className="hover:text-gray-500" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faGithub} size="2x" />
+                </a>
+              </li>
+              <li>
+                <a href="https://instagram.com/tristangjarrett" className="hover:text-pink-500" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faInstagram} size="2x" />
+                </a>
+              </li>
+            </ul>
+
+            <div className="mt-12">
+              <h2 className="text-2xl font-bold mb-6">What am I up to?</h2>
+              <ul className="flex space-x-4 mb-8">
+                {[
+                  { href: 'https://dysrupt.co.uk', src: '/dysrupt.jpeg', alt: 'Dysrupt' },
+                  { href: 'https://solution17.co.uk', src: '/solution17.jpeg', alt: 'Solution17' },
+                  { href: 'https://fyiro.io', src: '/fyiro.png', alt: 'Fyiro' },
+                  { href: 'https://nftbulq.com', src: '/bulq.png', alt: 'BulQ' },
+                ].map(({ href, src, alt }) => (
+                  <li key={alt} className="group relative">
+                    <a href={href} target="_blank" rel="noopener noreferrer" className="block">
+                      <div className="relative">
+                        <Image src={src} alt={alt} width={100} height={100} className="rounded-lg" />
+                        <div className="absolute inset-0 bg-black bg-opacity-40 opacity-0 group-hover:opacity-100 transition-opacity rounded-lg flex items-center justify-center">
+                          <FontAwesomeIcon icon={faExternalLinkAlt} size="lg" className="text-white" />
+                        </div>
+                      </div>
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <div id="contact" className="mt-12">
+              <h2 className="text-2xl font-bold mb-6">Get in touch</h2>
+              <form
+                onSubmit={handleSubmit(onSubmit)}
+                id="contact-form"
+                className="w-full mx-auto p-6 bg-white rounded-xl shadow-lg"
+              >
+                <div className="mb-6 w-full">
+                  <label htmlFor="name" className="block text-gray-700 font-semibold mb-2">
+                    Name
+                  </label>
+                  <input
+                    {...register('name', { required: true })}
+                    id="name"
+                    type="text"
+                    className="border-2 border-gray-300 px-4 py-2 w-full rounded-md focus:border-purple-500 focus:outline-none transition-colors"
+                  />
+                  {errors.name && (
+                    <p className="text-red-500 mt-2">This field is required</p>
+                  )}
+                </div>
+                <div className="mb-6 w-full">
+                  <label htmlFor="email" className="block text-gray-700 font-semibold mb-2">
+                    Email
+                  </label>
+                  <input
+                    {...register('email', { required: true, pattern: /^\S+@\S+$/i })}
+                    id="email"
+                    type="email"
+                    className="border-2 border-gray-300 px-4 py-2 w-full rounded-md focus:border-purple-500 focus:outline-none transition-colors"
+                  />
+                  {errors.email && (
+                    <p className="text-red-500 mt-2">
+                      Please enter a valid email address
+                    </p>
+                  )}
+                </div>
+                <div className="mb-6 w-full">
+                  <label htmlFor="message" className="block text-gray-700 font-semibold mb-2">
+                    Message
+                  </label>
+                  <textarea
+                    {...register('message', { required: true })}
+                    id="message"
+                    rows="5"
+                    className="border-2 border-gray-300 px-4 py-2 w-full rounded-md focus:border-purple-500 focus:outline-none transition-colors"
+                  ></textarea>
+                  {errors.message && (
+                    <p className="text-red-500 mt-2">This field is required</p>
+                  )}
+                </div>
+                <button
+                  type="submit"
+                  className="bg-gradient-to-r from-blue-500 via-purple-500 to-indigo-500 text-white px-4 py-4 rounded-md hover:from-blue-400 hover:via-purple-400 hover:to-indigo-400 w-full"
+                >
+                  Send message
+                </button>
+                {isSuccess && (
+                  <p className="text-green-500 mt-6">Your message was sent successfully!</p>
+                )}
+              </form>
+            </div>
+
+          </div>
+          <div className="w-full lg:w-1/3 mb-10 lg:mb-0 relative sticky top-10 hidden lg:block">
+            <div className="bg-gradient-to-br from-yellow-500 via-orange-500 to-pink-500 w-full h-full rounded-3xl shadow-lg absolute -left-4 -bottom-4 z-0"></div>
+            <Image
+              src="/tristan.jpeg"
+              alt="Tristan"
+              width={500}
+              height={500}
+              className="rounded-3xl shadow-lg relative z-10"
+            />
+          </div>
+
+        </div>
+
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <footer className="bg-gray-800 text-white py-4 text-center">
+        <p>
+          &copy; {new Date().getFullYear()} Tristan Jarrett
+        </p>
+      </footer>
+    </div>
+  );
+};
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Home;
