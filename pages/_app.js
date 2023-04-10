8ec1a308fe289850a9cadbdb236e210152be4b97
Tristan Jarrett <tristan.jarrett@outlook.com>
2023-04-10 10:15:17 +0100

    Add GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
 import '@/styles/globals.css'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=UA-137493871-1"></Script>
+      <Script>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'UA-137493871-1');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
