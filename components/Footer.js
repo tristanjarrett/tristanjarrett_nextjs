12a734cc86b2a597b1f77baa56b222ff475ddbb8
Tristan Jarrett <tristan.jarrett@outlook.com>
2023-04-13 11:34:02 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -10,9 +10,36 @@ const Footer = () => {
         </p>
         <p className='text-gray-500'>
         <a href="/privacy" className="hover:underline">Privacy Policy</a>
+        {' | '}
+        <a href="https://ko-fi.com/tristanjarrett" className="hover:underline">Ko-Fi</a>
         </p>
     </footer>
   );
 };
 
 export default Footer;
+
+// v2
+// import React from 'react';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faCoffee, faHeart } from '@fortawesome/free-solid-svg-icons';
+
+// const Footer = () => {
+//   return (
+//     <footer className="bg-gray-900 text-white">
+//       <div className="container mx-auto py-8 px-4 flex flex-wrap justify-between items-center">
+//         <div className="flex items-center">
+//           <p className="text-lg">&copy; {new Date().getFullYear()} Tristan Jarrett, made with <FontAwesomeIcon icon={faHeart} size="lg" style={{ color: 'red' }} /></p>
+//         </div>
+//         <div className="flex items-center space-x-4">
+//           <a href="/privacy" className="text-gray-400 hover:text-white">Privacy Policy</a>
+//           <a href="https://ko-fi.com/tristanjarrett" className="bg-yellow-400 hover:bg-yellow-500 text-gray-900 font-bold py-2 px-4 rounded flex items-center">
+//             <FontAwesomeIcon icon={faCoffee} size="lg" className="mr-2" /> Ko-Fi
+//           </a>
+//         </div>
+//       </div>
+//     </footer>
+//   );
+// };
+
+// export default Footer;
