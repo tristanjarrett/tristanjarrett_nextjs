3871e86af98b8b69064503662659cee7865f378a
Tristan Jarrett <tristan.jarrett@outlook.com>
2023-04-14 13:09:30 +0100

    Add icon

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Image from 'next/image'
+import Icon from '@/public/icon.svg'
 
 const Header = () => {
   return (
     <nav className="bg-gradient-to-r from-blue-500 via-purple-500 to-indigo-500 py-4 px-6 w-full flex justify-between items-center shadow-lg">
       <div className="flex items-center">
+        <Image src={Icon} width={40} height={40} className="mr-4" />
         <span className="text-xl font-semibold text-white"><a href="/">Tristan Jarrett</a></span>
       </div>
       <a href="/#contact" className="text-xl font-medium text-white hover:underline">
