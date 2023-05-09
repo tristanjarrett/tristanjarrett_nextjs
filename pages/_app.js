00e6b0eabf23682c3e473b2c317240bb131e6fef
Tristan Jarrett <tristan.jarrett@outlook.com>
2023-05-09 09:54:30 +0100

    GA v4

--- pages/_app.js
+++ pages/_app.js
@@ -4,13 +4,13 @@ import Script from 'next/script'
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=UA-137493871-1"></Script>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-V4SKVPGW7X"></Script>
       <Script>
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'UA-137493871-1');
+          gtag('config', 'G-V4SKVPGW7X');
         `}
       </Script>
       <Script src="https://www.google.com/recaptcha/api.js?render=6Ld0knQlAAAAACK-5SFSB5-VrsfIeQTI4KXd8O6T"></Script>
