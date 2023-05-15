24a265385a40b2f5b4e7c147e9691b42b2a94c4a
Tristan Jarrett <tristan.jarrett@outlook.com>
2023-05-16 00:15:53 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
   return (
     <footer className="pb-6 text-center">
         <p className='text-lg text-gray-800 dark:text-white'>
-        &copy; {new Date().getFullYear()} Tristan Jarrett, made with <FontAwesomeIcon icon={faCoffee} size="1x" />
+        &copy; {new Date().getFullYear()} Tristan Jarrett, made with <FontAwesomeIcon icon={faHeart} size="1x" />
         </p>
         <p className='text-gray-800 dark:text-gray-200'>
           <a href="/privacy" className="hover:underline">Privacy Policy</a>
