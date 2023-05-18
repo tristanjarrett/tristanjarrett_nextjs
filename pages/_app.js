11ed417b7ce6e0cdf0f558c38ca004faad9f8168
Tristan Jarrett <tristan.jarrett@outlook.com>
2023-05-18 19:21:22 +0100

    Cookie conditional render

--- pages/_app.js
+++ pages/_app.js
@@ -20,19 +20,21 @@ const CookieNotice = () => {
   }, []);
 
   return (
-    <div className={`fixed bottom-4 left-4 p-4 rounded-xl shadow-lg transition-opacity duration-300 ease-in-out opacity-0 ${isVisible ? 'opacity-100' : ''} bg-white dark:bg-gray-800 w-72 sm:w-auto`}>
-      <div className="flex items-center justify-center sm:justify-start">
-        <span className="mr-2">
-          <FontAwesomeIcon icon={faCookieBite} size="2x" />
-        </span>
-        <span className="mr-2">
-          I eat cookies, do you want some?
-        </span>
-        <button className="bg-gradient-to-br from-blue-500 via-purple-500 to-indigo-500 text-white font-bold px-4 py-2 rounded-md hover:from-blue-600 hover:via-purple-600 hover:to-indigo-600 dark:from-yellow-500 dark:via-orange-500 dark:to-pink-500 dark:hover:from-yellow-400 dark:hover:via-orange-400 dark:hover:to-pink-400" onClick={hideCookieNotice}>
-          Sure!
-        </button>
+    isVisible && (
+      <div className={`fixed bottom-4 left-4 p-4 rounded-xl shadow-lg bg-white dark:bg-gray-800 w-72 sm:w-auto`}>
+        <div className="flex items-center justify-center sm:justify-start">
+          <span className="mr-2">
+            <FontAwesomeIcon icon={faCookieBite} size="2x" />
+          </span>
+          <span className="mr-2">
+            I eat cookies, do you want some?
+          </span>
+          <button className="bg-gradient-to-br from-blue-500 via-purple-500 to-indigo-500 text-white font-bold px-4 py-2 rounded-md hover:from-blue-600 hover:via-purple-600 hover:to-indigo-600 dark:from-yellow-500 dark:via-orange-500 dark:to-pink-500 dark:hover:from-yellow-400 dark:hover:via-orange-400 dark:hover:to-pink-400" onClick={hideCookieNotice}>
+            Sure!
+          </button>
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
